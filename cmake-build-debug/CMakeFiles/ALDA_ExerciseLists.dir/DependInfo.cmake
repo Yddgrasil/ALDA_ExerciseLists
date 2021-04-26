8369e03462c8de4d8c1ef00136e7834b3e337ca3
Yddgrasil <71716001+Yddgrasil@users.noreply.github.com>
2021-04-26 20:23:56 +0200

    Testing the Lion

---
+++ cmake-build-debug/CMakeFiles/ALDA_ExerciseLists.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Dani/CLionProjects/ALDA_ExerciseLists/list.c" "C:/Users/Dani/CLionProjects/ALDA_ExerciseLists/cmake-build-debug/CMakeFiles/ALDA_ExerciseLists.dir/list.c.obj"
+  "C:/Users/Dani/CLionProjects/ALDA_ExerciseLists/main.c" "C:/Users/Dani/CLionProjects/ALDA_ExerciseLists/cmake-build-debug/CMakeFiles/ALDA_ExerciseLists.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ALDA_ExerciseLists.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "ALDA_ExerciseLists.exe"
+  "ALDA_ExerciseLists.exe.manifest"
+  "ALDA_ExerciseLists.lib"
+  "ALDA_ExerciseLists.pdb"
+  "CMakeFiles/ALDA_ExerciseLists.dir/list.c.obj"
+  "CMakeFiles/ALDA_ExerciseLists.dir/main.c.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ALDA_ExerciseLists.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
